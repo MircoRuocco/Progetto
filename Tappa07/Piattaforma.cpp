66eedea9b4a06d6bd2d1ed4fa568f721da65e5bf
MircoRuocco <167685031+MircoRuocco@users.noreply.github.com>
2026-09-07 19:11:26 +0200

    Add files via upload

---
+++ Tappa07/Piattaforma.cpp
@@ -0,0 +1,41 @@
+#include "Piattaforma.hpp"
+
+Piattaforma::Piattaforma(float x, float y)
+    : shape({150.f, 30.f}),
+      posizioneIniziale({x, y})
+{
+    shape.setFillColor(sf::Color::Magenta);
+
+    shape.setPosition(posizioneIniziale);
+}
+
+
+void Piattaforma::aggiorna(float deltaTime, float velocitaLivello)
+{
+
+    shape.move({
+        -velocitaLivello * deltaTime,
+        0.f
+    });
+}
+
+
+void Piattaforma::reset()
+{
+    shape.setPosition(posizioneIniziale);
+}
+
+
+
+sf::FloatRect Piattaforma::getBounds() const
+{
+    return shape.getGlobalBounds();
+}
+
+
+
+void Piattaforma::disegna(
+    sf::RenderWindow& window) const
+{
+    window.draw(shape);
+}
